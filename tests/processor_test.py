484197eab3cfbe5a964180187142a8365adbc704
liguobao <liguobao@nana-tech.com>
2018-11-12 23:25:03 +0800

    update tests

--- tests/processor_test.py
+++ tests/processor_test.py
@@ -5,6 +5,7 @@ import unittest
 from unittest.mock import MagicMock
 from unittest.mock import patch
 from processor import Processor
+import pika
 
 
 class TestProcessor(unittest.TestCase):
@@ -27,11 +28,32 @@ class TestProcessor(unittest.TestCase):
         key = test_processor._get_queue()
         self.assertEqual(key, "run.processor.test")
 
-    # @patch("function.connection.channel")
-    # def test_init_channel(self, channel):
-    #     test_processor = Processor("guest:guest@127.0.0.1:5672", "test", None)
-    #     channel = None
-    #     test_processor._channel
+    @patch("pika.BlockingConnection")
+    @patch("pika.BlockingConnection.channel")
+    def test_init_channel(self, mock_connection, mock_channel):
+        backpressure = "%2F?backpressure_detection=t"
+        urlParameter = "amqp://%s/%s" % (
+            "guest:guest@127.0.0.1:5672", backpressure)
+        parameters = pika.URLParameters(urlParameter)
+        connection = pika.BlockingConnection(parameters)
+        mock_connection.return_value = connection
+        # mock_channel.return_value = connection.channel()
+        test_processor = Processor("", "test", None)
+        self.assertEqual(test_processor._channel, connection.channel())
+
+    @patch("pika.BlockingConnection")
+    @patch("pika.BlockingConnection.channel")
+    def test_callback(self, mock_connection, mock_channel):
+        backpressure = "%2F?backpressure_detection=t"
+        urlParameter = "amqp://%s/%s" % (
+            "guest:guest@127.0.0.1:5672", backpressure)
+        parameters = pika.URLParameters(urlParameter)
+        connection = pika.BlockingConnection(parameters)
+        mock_connection.return_value = connection
+        # mock_channel.return_value = connection.channel()
+        test_processor = Processor("", "test", None)
+        self.assertEqual(test_processor._callback, None)
+
 
 if __name__ == '__main__':
     unittest.main()
